49fe62b6490baefda19d5730e667770d35c623ca
Kaan Caliskan <kaancaliskan@Kaans-MacBook-Air.local>
2019-01-02 18:54:58 -0800

    added rtmpserver

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,7 +19,7 @@ const App = () => {
             <Route path="/streams/new" component={StreamCreate} />
             <Route path="/streams/edit/:id" component={StreamEdit} />
             <Route path="/streams/delete/:id" component={StreamDelete} />
-            <Route path="/streams/show/:id" component={StreamShow} />
+            <Route path="/streams/:id" component={StreamShow} />
           </Switch>
         </div>
       </Router>
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -2,17 +2,26 @@ import React from "react";
 import { fetchStream } from "../../actions";
 import { connect } from "react-redux";
 
-class StreamShow {
-  componentDidMount() {}
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
+
+    return (
+      <div>
+        <h1>{this.props.stream.title}</h1>
+        <h5>{this.props.stream.description}</h5>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return { stream: state.streams[ownProps.params.match.id] };
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
 export default connect(
